eab23e48030e8c161e924be52330af0060aee0cd
Max <maxpower@riseup.net>
2019-07-13 20:01:56 -0300

    Agregado ActiveAdmin con Devise en castellano

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Observatorio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :es
+
+    # Guardar en hora local
+    config.time_zone = 'Buenos Aires'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
