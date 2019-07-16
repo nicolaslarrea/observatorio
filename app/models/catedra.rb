56eaab215191afaa9011f5aafd14ba21cd0d5e3a
Max <maxpower@riseup.net>
2019-07-15 22:43:24 -0300

    FIX para error de asociación en Modelo Catedra

--- app/models/catedra.rb
+++ app/models/catedra.rb
@@ -1,3 +1,7 @@
 class Catedra < ApplicationRecord
   belongs_to :materia
+  has_many :oferta_en_listas
+  has_many :inscripciones, through: :oferta_en_listas
+
+  validates :codigo, presence: true, uniqueness: true
 end
