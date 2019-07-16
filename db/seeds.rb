33d7693d297c45be1217fc85bc1db9c5bc39f36f
Max <maxpower@riseup.net>
2019-07-15 22:43:55 -0300

    Seeds para carreras

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# Crear las carreras que no pueden ser generadas por scrapeo.
+[['Lic. en Psicología','PS'],['Prof. en Psicología','PR'],['Lic. en Musicoterapia','LM'],['Lic. en Terapia Ocupacional','TE']].each do | nombre, abreviatura|
+  Carrera.create(nombre: nombre , abreviatura: abreviatura )
+end
